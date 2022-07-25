1c43a3d0797cab33d288d027127d7d756ff16a8f
adammcphail12 <99302328+adammcphail12@users.noreply.github.com>
2022-07-26 09:22:43 +1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Put Content Here">
+    <meta name="keywords" content="Put keywords here">
+    <meta name="author" content="Put your name here">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Adams Food Reviews</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="css/bookstyle.css"> 
+    
+</head>
+    
+<body>
+    
+    <div class="wrapper">
+    
+
+        
+        <div class="box banner">
+            
+        <!-- logo image linking to home page goes here -->
+        <a href="index.php">
+            <div class="box logo"  title="Logo - Click here to go to the Home Page">
+            <img class="img-circle" src="images/Adams Food Reviews.png" width="150" height="105" alt="generic logo" />
+            
+            </div>    <!-- / logo -->
+        </a>
+            
+            <h1>Adams Food Reviews</h1>
+        </div>    <!-- / banner -->
+
+        <!-- Navigation goes here.  Edit BOTH the file name and the link name -->
+            
+        
+        
+        <div class="box side">
+            Search
+            Name Search
+            Meal Type Search
+            Location Search
+            Rating Search
+        </div>
+        
+        <div class="box main">
+            <h2>First Page heading</h2>
+            
+            <p>Adams Food Reviews is a page dedicated to giving complete food reviews on different sort of meals. This includes, takeout, restaurants (local to new zealand) and home cooked meals. </p>
+
+            <p>Food on this site has been reviewed in majority by Ms Gottschalk, however one meal has been reviewed by me, Adam McPhail. </p>
+        </div>    <!-- / main -->
+        
+
+        <div class="box footer">
+            Adams 2022
+        </div>    <!-- / footer -->
+    
+    </div>  <!-- / wrapper  -->
+    
+</body>        
